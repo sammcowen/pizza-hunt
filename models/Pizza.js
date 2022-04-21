f3b7e97b86b21f144026a38ea7870ea50543dfdf
HBK <sammcowen@gmail.com>
2022-04-20 17:25:53 -0700

    lesson 2 code

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,25 +1,42 @@
-const {Schema,model} = require('mongoose');
+const { Schema, model } = require('mongoose');
 
 const PizzaSchema = new Schema({
-pizzaName:{
-    type:String,
-},
-createdBy: {
-    type:String,
-},
-createdAt: {
-    type:Date,
-    default:Date.now,
-},
-size:{
-    type:String,
-    default:'Large',
+    pizzaName: {
+        type: String,
+    },
+    createdBy: {
+        type: String,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    size: {
+        type: String,
+        default: 'Large',
 
+    },
+    toppings: [],
+    comments: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ]
 },
-toppings:[]
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
+    }
 
+);
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function () {
+    return this.comments.length;
 });
 // create Pizza Model using Pizza Schema 
-const Pizza = model('Pizza',PizzaSchema);
+const Pizza = model('Pizza', PizzaSchema);
 // export pizza model 
 module.exports = Pizza;
